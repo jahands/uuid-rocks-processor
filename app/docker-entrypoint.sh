1a3cd6c36262f9a1b2ebb0a06b4904a908fb2300
Jacob Hands <jacob@gogit.io>
2022-11-06 11:24:44 -0600

    Try to reset mem usage

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 while :
 do
+    # Skip sleep on first run
+    if test -f /tmp/railway_not_first_run; then
+      sleep 5.75h # BigQuery imports every 6 hours - this gives us a little buffer (15 minutes)
+    else
+      touch /tmp/railway_not_first_run
+    fi
     echo Processing data... \
     && date \
     && pwsh -File $HOME/sync.ps1 \
     && date \
     && echo Sending heartbeat to BetterUptime... \
     && curl -X POST $BETTERUPTIME_URL
-    sleep 5.75h # BigQuery imports every 6 hours - this gives us a little buffer (15 minutes)
+    # Reset memory usage
+    echo Resetting memory usage by crashing
+    exit 1
 done
