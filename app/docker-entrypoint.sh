4fc7eae2332d6127c7e9a6e0b0437828d064fb17
Jacob Hands <jacob@gogit.io>
2022-11-06 11:27:10 -0600

    Add echo

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -3,6 +3,7 @@ while :
 do
     # Skip sleep on first run
     if test -f /tmp/railway_not_first_run; then
+      echo Sleeping for 5.75h
       sleep 5.75h # BigQuery imports every 6 hours - this gives us a little buffer (15 minutes)
     else
       touch /tmp/railway_not_first_run
